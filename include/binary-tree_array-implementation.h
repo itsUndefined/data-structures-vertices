dca28825977d7f2c01db4cea2994a06b666b31e4
Tsintzask <tsintzask6@gmail.com>
2018-04-22 14:33:56 +0300

    First Commit

---
+++ include/binary-tree_array-implementation.h
@@ -0,0 +1,16 @@
+#ifndef BINARY-TREE_ARRAY-IMPLEMENTATION_H
+#define BINARY-TREE_ARRAY-IMPLEMENTATION_H
+
+
+class binary-tree_array-implementation
+{
+    public:
+        binary-tree_array-implementation();
+        virtual ~binary-tree_array-implementation();
+
+    protected:
+
+    private:
+};
+
+#endif // BINARY-TREE_ARRAY-IMPLEMENTATION_H
---
+++ src/binary-tree_array-implementation.cpp
@@ -0,0 +1,11 @@
+#include "binary-tree_array-implementation.h"
+
+binary-tree_array-implementation::binary-tree_array-implementation()
+{
+    //ctor
+}
+
+binary-tree_array-implementation::~binary-tree_array-implementation()
+{
+    //dtor
+}
