11fab6e3e049f4e6d49adeb5369bc05327c18d1b
tsintzask <36897465+tsintzask@users.noreply.github.com>
2018-05-09 02:19:24 +0300

    Updated Header file for Arrays

--- include/Array.h
+++ include/Array.h
@@ -1,16 +1,21 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-
-class Array
-{
+class Array{
     public:
         Array();
+        Array(int nodes);
         virtual ~Array();
-
+        void insertLink(int x, int y);
+        void deleteLink(int x, int y);
+        int *findNeighbors(int x, int &num);
+        int /*bit*/connectedComponents();//BITCONNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEECT
+        void print(ofstream s);
     protected:
-
-    private:
+        int **A;//main array
+        int *neighbors, *next;//helper arrays for the individual elements
+        int len, maximum;//helper variables for the main array, A
+        void expand(int id, int x);//x only relevant when id is -1, indicating the expansion of A instead of a sub-table
 };
 
 #endif // ARRAY_H
